e8a34cabd5afc714d1b479ef3e95f47d9763f50c
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-28 17:38:23 -0500

    Simulating data manipulation for front end display

--- schedule.rb
+++ schedule.rb
@@ -16,4 +16,36 @@ response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
 end
 
 team_schedule = JSON.parse(response.body)
-pp team_schedule
+
+# Data example
+# {"id"=>401281942,
+#   "season"=>2021,
+#   "week"=>1,
+#   "season_type"=>"regular",
+#   "start_date"=>"2021-09-04T19:30:00.000Z",
+#   "start_time_tbd"=>false,
+#   "neutral_site"=>true,
+#   "conference_game"=>false,
+#   "attendance"=>nil,
+#   "venue_id"=>5348,
+#   "venue"=>"Mercedes-Benz Stadium",
+#   "home_id"=>2390,
+#   "home_team"=>"Miami",
+#   "home_conference"=>"ACC",
+#   "home_points"=>13,
+#   "home_line_scores"=>[0, 3, 10, 0],
+#   "home_post_win_prob"=>"0.01176962249522609",
+#   "away_id"=>333,
+#   "away_team"=>"Alabama",
+#   "away_conference"=>"SEC",
+#   "away_points"=>44,
+#   "away_line_scores"=>[10, 17, 14, 3],
+#   "away_post_win_prob"=>"0.9882303775047739",
+#   "excitement_index"=>"0.7900699466",
+#   "highlights"=>nil,
+#   "notes"=>nil},
+# }
+
+front_end = team_schedule.map { |game| "Week #{game["week"]}: #{game["away_team"]} at #{game["home_team"]}, Final score: #{game["away_points"]} - #{game["home_points"]}" }
+
+pp front_end
