09ade06e02fb6824cd127d2dbae2132ad374a67b
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-29 15:18:40 -0500

    Added team roster function to get the roster of a specific team for a specific year

--- current_roster_refined.rb
+++ current_roster_refined.rb
@@ -1,3 +1,21 @@
 require "net/http"
 require "json"
 require "uri"
+
+def team_roster(team, year)
+  uri = URI.parse("https://api.collegefootballdata.com/roster?team=#{team}&year=#{year}")
+  request = Net::HTTP::Get.new(uri)
+  request["Accept"] = "application/json"
+  request["Authorization"] = "Bearer "
+
+  req_options = {
+    use_ssl: uri.scheme == "https",
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
+
+  team_roster = JSON.parse(response.body)
+  return team_roster
+end
