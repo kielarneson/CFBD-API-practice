c023c05ee80b547f63f3392a979a8e5f46800a6c
Kiel Arneson <kiel.arneson@gmail.com>
2024-07-04 18:56:15 -0500

    changed some variable names to make code more readable

--- college_to_nfl_draft_rankings_compared.rb
+++ college_to_nfl_draft_rankings_compared.rb
@@ -21,11 +21,11 @@ def team_recruits_by_year(team, year)
   return team_recruits
 end
 
-recruits_on_specific_cfb_team_from2019to2023 = (team_recruits_by_year("Georgia", 2020) | 
-                                                team_recruits_by_year("Georgia", 2019) |
-                                                team_recruits_by_year("Georgia", 2018) |
-                                                team_recruits_by_year("Georgia", 2017) |
-                                                team_recruits_by_year("Georgia", 2016))
+recruits_on_specific_team_from2019to2023 = (team_recruits_by_year("Alabama", 2020) | 
+                                            team_recruits_by_year("Alabama", 2019) |
+                                            team_recruits_by_year("Alabama", 2018) |
+                                            team_recruits_by_year("Alabama", 2017) |
+                                            team_recruits_by_year("Alabama", 2016))
 
 def nfl_draft_class_by_year(year)
   uri = URI.parse("https://api.collegefootballdata.com/draft/picks?year=#{year}")
@@ -52,42 +52,42 @@ nfl_draft_classes_from2020to2024 = (nfl_draft_class_by_year(2023) |
                                     nfl_draft_class_by_year(2020) | 
                                     nfl_draft_class_by_year(2019))
 
-# pp recruits_on_specific_cfb_team_from2019to2023
+# pp recruits_on_specific_team_from2019to2023
 # pp nfl_draft_classes_from2020to2024
                                     
-def player_information_for_nfl_draft_picks_from_specific_cfb_team_over_specific_timespan(players, draft_class)
-  players_in_nfl = []
+def player_information_for_nfl_draft_picks_from_specific_cfb_team_over_specific_timespan(players_from_team, draft_classes)
+  players_from_team_drafted_in_nfl = []
   index1 = 0
   index2 = 0
 
-  while index1 < players.length
-    while index2 < draft_class.length
-      if players[index1]["athleteId"].to_i == draft_class[index2]["collegeAthleteId"] && players[index1]["committedTo"] == draft_class[index2]["collegeTeam"]
+  while index1 < players_from_team.length
+    while index2 < draft_classes.length
+      if players_from_team[index1]["athleteId"].to_i == draft_classes[index2]["collegeAthleteId"] && players_from_team[index1]["committedTo"] == draft_classes[index2]["collegeTeam"]
 
-        players_in_nfl << {"name" => players[index1]["name"],
-                           "position" => players[index1]["position"],
-                           "committedTo" => players[index1]["committedTo"],
-                           "year" => players[index1]["year"],
-                           "highSchoolHeight" => players[index1]["height"],
-                           "height" => draft_class[index2]["height"],
-                           "highSchoolWeight" => players[index1]["weight"],
-                           "weight" => draft_class[index2]["weight"],
-                           "school" => players[index1]["school"],
-                           "city" => players[index1]["city"],
-                           "stateProvince" => players[index1]["stateProvince"],
-                           "country" => players[index1]["country"],
-                           "stars" => players[index1]["stars"],
-                           "rating" => players[index1]["rating"],
-                           "ranking" => players[index1]["ranking"],
-                           "draftedBy" => draft_class[index2]["nflTeam"],
-                           "draftYear" => draft_class[index2]["year"],
-                           "overall" => draft_class[index2]["overall"],
-                           "round" => draft_class[index2]["round"],
-                           "pick" => draft_class[index2]["pick"],
-                           "preDraftRanking" => draft_class[index2]["preDraftRanking"],
-                           "preDraftPositionRanking" => draft_class[index2]["preDraftPositionRanking"],
-                           "preDraftGrade" => draft_class[index2]["preDraftGrade"],
-                           "collegeToProRankingChange" => players[index1]["ranking"] - draft_class[index2]["overall"]}
+        players_from_team_drafted_in_nfl << {"name" => players_from_team[index1]["name"],
+                                              "position" => players_from_team[index1]["position"],
+                                              "year" => players_from_team[index1]["year"],
+                                              "committedTo" => players_from_team[index1]["committedTo"],
+                                              "highSchoolHeight" => players_from_team[index1]["height"],
+                                              "height" => draft_classes[index2]["height"],
+                                              "highSchoolWeight" => players_from_team[index1]["weight"],
+                                              "weight" => draft_classes[index2]["weight"],
+                                              "school" => players_from_team[index1]["school"],
+                                              "city" => players_from_team[index1]["city"],
+                                              "stateProvince" => players_from_team[index1]["stateProvince"],
+                                              "country" => players_from_team[index1]["country"],
+                                              "stars" => players_from_team[index1]["stars"],
+                                              "rating" => players_from_team[index1]["rating"],
+                                              "ranking" => players_from_team[index1]["ranking"],
+                                              "draftedBy" => draft_classes[index2]["nflTeam"],
+                                              "draftYear" => draft_classes[index2]["year"],
+                                              "overall" => draft_classes[index2]["overall"],
+                                              "round" => draft_classes[index2]["round"],
+                                              "pick" => draft_classes[index2]["pick"],
+                                              "preDraftRanking" => draft_classes[index2]["preDraftRanking"],
+                                              "preDraftPositionRanking" => draft_classes[index2]["preDraftPositionRanking"],
+                                              "preDraftGrade" => draft_classes[index2]["preDraftGrade"],
+                                              "collegeToProRankingChange" => players_from_team[index1]["ranking"] - draft_classes[index2]["overall"]}  
       end
       index2 += 1
     end
@@ -95,8 +95,8 @@ def player_information_for_nfl_draft_picks_from_specific_cfb_team_over_specific_
     index2 = 0
   end
 
-  return players_in_nfl
+  return players_from_team_drafted_in_nfl
 end
 
-pp player_information_for_nfl_draft_picks_from_specific_cfb_team_over_specific_timespan(recruits_on_specific_cfb_team_from2019to2023, nfl_draft_classes_from2020to2024)
+pp player_information_for_nfl_draft_picks_from_specific_cfb_team_over_specific_timespan(recruits_on_specific_team_from2019to2023, nfl_draft_classes_from2020to2024)
 
